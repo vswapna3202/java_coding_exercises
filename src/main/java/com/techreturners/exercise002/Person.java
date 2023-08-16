4d1abf65f274292ff7ace976e1ffb49f7d6a07c5
Swapna V <vswapna3202@gmail.com>
2023-08-16 16:27:55 +0100

    Feat: Changed person class to have all methods

--- src/main/java/com/techreturners/exercise002/Person.java
+++ src/main/java/com/techreturners/exercise002/Person.java
@@ -17,11 +17,32 @@ public class Person {
         this.age = age;
     }
 
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+
+    public void setCity(String city){
+        this.city = city;
+    }
     public String getCity() {
         return city;
     }
 
-
+    public void setAge(int age){
+        this.age = age;
+    }
     public int getAge() {
         return age;
     }
